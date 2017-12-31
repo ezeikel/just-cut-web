913e1f53aca023917c88e698e533878e5813c7f3
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2017-12-31 01:30:16 +0000

    Added eslint to project and pre-commit hook.

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,10 @@ const shopController = require('../controllers/shopController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // do work here
-router.get('/', (req, res, next) => res.send('Hey! It works'));
-router.post('/add', catchErrors(shopController.createShop));
+router.get('/', (req, res, next) => res.send('The Server is running. Run the React App.'));
 router.get('/users', catchErrors(userController.testApi));
 router.get('/shops', catchErrors(shopController.getShops));
+
+router.post('/add', catchErrors(shopController.createShop));
 
 module.exports = router;
