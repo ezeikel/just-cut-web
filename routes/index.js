b0fa39f991982f853c5e0afbeff8ddb878bab5c5
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2017-12-31 01:30:42 +0000

    Removed unused var.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
+
 const router = express.Router();
 const userController = require('../controllers/userController');
 const shopController = require('../controllers/shopController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
-// do work here
-router.get('/', (req, res, next) => res.send('The Server is running. Run the React App.'));
+router.get('/', (req, res) => res.send('The Server is running. Run the React App.'));
 router.get('/users', catchErrors(userController.testApi));
 router.get('/shops', catchErrors(shopController.getShops));
 
