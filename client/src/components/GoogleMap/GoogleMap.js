6de8efa344dd1a1dacb65f9ca4b7539dba3e6e9f
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-01-29 01:38:40 +0000

    Google api key changed.

--- client/src/components/GoogleMap/GoogleMap.js
+++ client/src/components/GoogleMap/GoogleMap.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 
 class GoogleMap extends Component {
+    shouldComponentUpdate() {
+        return false;
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.map.panTo({ lat: nextProps.lat, lng: nextProps.lng });
+    }
+
     componentDidMount() {
        const shop = {lat: this.props.lat, lng: this.props.lng};
-       const map = new google.maps.Map(this.refs.map, { // eslint-disable-line
+       this.map = new google.maps.Map(this.refs.map, { // eslint-disable-line
            zoom: 15,
            center: shop
        });
 
        new google.maps.Marker({ // eslint-disable-line
            position: shop,
-           map
+           map: this.map
        });
     }
 
