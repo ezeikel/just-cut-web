f805d6eb6826426b2e4096d3fa4a314477ae94f6
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-01-29 21:44:21 +0000

    More node env checks.

--- start.js
+++ start.js
@@ -18,4 +18,5 @@ const app = require('./app');
 app.set('port', process.env.PORT || 7777);
 const server = app.listen(app.get('port'), () => {
     console.log(`Express running → PORT ${server.address().port}`);
+    console.log(`Environment → ${app.get('env')}`);
 });
