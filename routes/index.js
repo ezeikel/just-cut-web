d72a8f56d2fcd34dd322032ab3452c68a522ec3b
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-01-30 01:27:29 +0000

    Removed / route from express.

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ const userController = require('../controllers/userController');
 const shopController = require('../controllers/shopController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
-router.get('/', (req, res) => res.send('The Server is running. Run the React App.'));
 router.get('/users', catchErrors(userController.testApi));
 router.get('/shops', catchErrors(shopController.getShops));
 router.post('/find-shops', catchErrors(shopController.findShops));
