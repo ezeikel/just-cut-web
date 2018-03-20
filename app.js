d912e727377c349a221010854cb6de4ceccb6e5c
Ezeikel Pemberton <ezeikel_pemberton@discovery.com>
2018-03-20 17:23:39 +0000

    Moved graphql schema and resolvers to their own file ~ spring cleaning.

--- app.js
+++ app.js
@@ -11,107 +11,10 @@ const promisify = require('es6-promisify');
 const expressValidator = require('express-validator');
 const routes = require('./routes/index');
 const helpers = require('./helpers');
-const aws = require('aws-sdk');
-const uuid = require('uuid');
+const { schema, root } = require('./graphql');
 // const errorHandlers = require('./handlers/errorHandlers');
 
-const { buildSchema } = require('graphql');
 const graphqlHTTP = require('express-graphql');
-
-// TODO: Probably need to move this related graphql stuff into a seperate graphql file
-const Shop = mongoose.model('Shop');
-
-// some fake data
-const books = [
-  {
-    title: "Harry Potter and the Sorcerer's stone",
-    author: 'J.K. Rowling',
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
-  },
-];
-
-// the GraphQL schema in string form
-const schema = buildSchema(`
-  type Book { title: String, author: String }
-  type Shop {
-    id: ID,
-    name: String,
-    slug: String,
-    description: String,
-    tags: [String],
-    location: Location,
-    distance: Float,
-    photo: String
-  }
-  input LocationInput {
-    coordinates: [Float],
-    address: String
-  }
-  type Location {
-    coordinates: [Float],
-    address: String
-  }
-  type S3Payload {
-    signedRequest: String!,
-    url: String!
-  }
-  type Query {
-    books: [Book!]!,
-    shops: [Shop!]!,
-    getShopBySlug(slug: String!): Shop,
-    findNearestShops(coordinates: [Float!]!): [Shop]
-  }
-  type Mutation {
-    createShop(name: String, location: LocationInput, photo: String): Shop,
-    signS3(filetype: String!): S3Payload!
-  }
-`);
-
-// the root provides a resolver function for each API endpoint
-const root = {
-  books: () => books,
-  shops: async () => Shop.find(),
-  createShop: async (obj) => new Shop(obj).save(),
-  getShopBySlug: async ({ slug }) => Shop.findOne({ slug }),
-  findNearestShops: async ({ coordinates }) => Shop.aggregate([
-    {
-      $geoNear: {
-        near: { type: 'Point', coordinates },
-        distanceMultiplier: 0.000621371,
-        distanceField: 'distance',
-        minDistance: 0,
-        maxDistance: 5 * 1609.344,
-        spherical: true
-      }
-    }
-  ]),
-  signS3: async ({ filetype }) => {
-    const extension = filetype.split('/')[1];
-    const name = `${uuid.v4()}.${extension}`;
-
-    const s3 = new aws.S3({
-      signatureVersion: 'v4',
-      region: 'eu-west-2'
-    });
-
-    const s3Params = {
-      Bucket: process.env.S3_BUCKET,
-      Key: `uploads/${name}`,
-      Expires: 60 * 5,
-      ContentType: filetype
-    };
-
-    const signedRequest = await s3.getSignedUrl('putObject', s3Params);
-    const url = `https://${process.env.S3_BUCKET}.s3.amazonaws.com/uploads/${name}`;
-    return {
-      signedRequest,
-      url
-    };
-  }
-};
 
 // kept from express-generator
 const favicon = require('serve-favicon');
---
+++ graphql.js
@@ -0,0 +1,84 @@
+const { buildSchema } = require('graphql');
+const aws = require('aws-sdk');
+const uuid = require('uuid');
+const mongoose = require('mongoose');
+
+// TODO: Probably need to move this related graphql stuff into a seperate graphql file
+const Shop = mongoose.model('Shop');
+
+// the GraphQL schema in string form
+module.exports.schema = buildSchema(`
+  type Shop {
+    id: ID,
+    name: String,
+    slug: String,
+    description: String,
+    tags: [String],
+    location: Location,
+    distance: Float,
+    photo: String
+  }
+  input LocationInput {
+    coordinates: [Float],
+    address: String
+  }
+  type Location {
+    coordinates: [Float],
+    address: String
+  }
+  type S3Payload {
+    signedRequest: String!,
+    url: String!
+  }
+  type Query {
+    shops: [Shop!]!,
+    getShopBySlug(slug: String!): Shop,
+    findNearestShops(coordinates: [Float!]!): [Shop]
+  }
+  type Mutation {
+    createShop(name: String, location: LocationInput, photo: String): Shop,
+    signS3(filetype: String!): S3Payload!
+  }
+`);
+
+// the root provides a resolver function for each API endpoint
+module.exports.root = {
+  shops: async () => Shop.find(),
+  createShop: async (obj) => new Shop(obj).save(),
+  getShopBySlug: async ({ slug }) => Shop.findOne({ slug }),
+  findNearestShops: async ({ coordinates }) => Shop.aggregate([
+    {
+      $geoNear: {
+        near: { type: 'Point', coordinates },
+        distanceMultiplier: 0.000621371,
+        distanceField: 'distance',
+        minDistance: 0,
+        maxDistance: 5 * 1609.344,
+        spherical: true
+      }
+    }
+  ]),
+  signS3: async ({ filetype }) => {
+    const extension = filetype.split('/')[1];
+    const name = `${uuid.v4()}.${extension}`;
+
+    const s3 = new aws.S3({
+      signatureVersion: 'v4',
+      region: 'eu-west-2'
+    });
+
+    const s3Params = {
+      Bucket: process.env.S3_BUCKET,
+      Key: `uploads/${name}`,
+      Expires: 60 * 5,
+      ContentType: filetype
+    };
+
+    const signedRequest = await s3.getSignedUrl('putObject', s3Params);
+    const url = `https://${process.env.S3_BUCKET}.s3.amazonaws.com/uploads/${name}`;
+    return {
+      signedRequest,
+      url
+    };
+  }
+};
