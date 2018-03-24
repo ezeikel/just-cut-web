68cb95c776322fe978859c3ad922c39680c2468e
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-03-24 03:03:24 +0000

    Centered text inside of tag.

--- client/src/components/Search/SearchResults/SearchResult/SearchResult.js
+++ client/src/components/Search/SearchResults/SearchResult/SearchResult.js
@@ -38,6 +38,9 @@ const SearchResultTags = styled.ul`
   grid-gap: var(--spacing-small);
   font-size: 14px;
   li {
+    display: grid;
+    justify-items: center;
+    align-items: center;
     padding: 4px;
     background-color: palevioletred;
     color: var(--color-white)
