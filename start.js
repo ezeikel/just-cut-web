1880fdb1bc1c48874bd72782db0d7302cd0d3f04
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-05-12 15:56:10 +0100

    Back to react favicon.

--- start.js
+++ start.js
@@ -7,21 +7,22 @@ require('dotenv').config({ path: 'variables.env' });
 mongoose.connect(process.env.DATABASE);
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 mongoose.connection.on('error', (err) => {
-    console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+  console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
 });
 
 // Load mongodb collections on start up
 require('./models/Shop');
 
 // scrambles a connection string, showing only relevant info
-const scramble = (connection_string) => connection_string.replace(/:\/\/.*?\//, "://***/");
+const scramble = (connectionString) => connectionString.replace(/:\/\/.*?\//, '://***/');
 
 // Start our app & notify console of server boot and current settings
 const app = require('./app');
+
 app.set('port', process.env.PORT || 7777);
 const server = app.listen(app.get('port'), () => {
-    console.log(`Just Cutt Express Server running ✂️✂️✂️`);
-    console.log(`PORT → ${server.address().port}`);
-    console.log(`ENVIRONMENT → ${app.get('env')}`);
-    console.log(`DATABASE → ${scramble(process.env.DATABASE)}`);
+  console.log(`Just Cutt Express Server running ✂️✂️✂️`);
+  console.log(`PORT → ${server.address().port}`);
+  console.log(`ENVIRONMENT → ${app.get('env')}`);
+  console.log(`DATABASE → ${scramble(process.env.DATABASE)}`);
 });
