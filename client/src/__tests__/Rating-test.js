13d91df6fdcd95b0fdf61d8b5d325ed2c6e1692c
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-05-21 02:52:12 +0100

    Some tests.

---
+++ client/src/__tests__/Rating-test.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import Rating from '../components/Rating/Rating';
+
+describe('Rating Component', () => {
+  //TODO: This test just seems to be passing
+  it('should render without throwing an error', () => {
+    expect(shallow(<Rating />).exists(<form />)).toBe(true)
+  });
+
+  it('renders five inputs', () => {
+    expect(mount(<Rating />).find('input').length).toEqual(5)
+  });
+
+  it('renders five labels', () => {
+    expect(mount(<Rating />).find('label').length).toEqual(5)
+  });
+
+  it('renders total ratings if readonly is true', () => {
+    expect(mount(<Rating readonly={true} />).find('span').length).toEqual(1)
+  });
+
+  it('renders submit if no readonly prop is passed in', () => {
+    expect(mount(<Rating />).find('button').length).toEqual(1);
+  });
+
+  it('doesnt render submit button if readonly is true', () => {
+    expect(mount(<Rating readonly={true} />).find('button').length).toEqual(0);
+  });
+
+  //TODO: Test to make sure this is working properly and commented out test below.
+  //TODO: Add snapshot test
+  it('default to no ratings', () => {
+    const wrapper = shallow(<Rating />);
+    const instance = wrapper.instance();
+    jest.spyOn(instance, 'totalRatings');
+    expect(instance.totalRatings()).toEqual('No ratings yet.');
+  });
+
+  // it('works out average rating', () => {
+  //   const wrapper = shallow(<Rating />);
+  //   const instance = wrapper.instance();
+  //   jest.spyOn(instance, 'totalRatings');
+  //   instance.calculateAverageRating([5, 5, 5]);
+  //   //expect(wrapper.state().rating).to.equal(5);
+  //   //expect(wrapper.state().foo).to.equal(10);
+  //   // console.log(instance.totalRatings);
+  //   expect(instance.totalRatings()).toEqual(5);
+  // });
+});
