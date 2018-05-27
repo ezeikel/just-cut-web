37da879c11a13b5cc262c9f64465b4f2a00cb0d2
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-05-27 02:38:36 +0100

    Added barebones Login component.

--- client/src/containers/Main/Main.js
+++ client/src/containers/Main/Main.js
@@ -4,11 +4,13 @@ import Home from '../Home/Home';
 import Shops from '../../components/Shops/Shops';
 import AddShop from '../../components/AddShop/AddShop';
 import Shop from '../../components/Shops/Shop/Shop';
+import Login from '../Login/Login';
 
 const Main = () => (
   <main>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route path="/login" component={Login} />
       <Route path="/shops" component={Shops} />
       <Route path="/shop/:slug" component={Shop} />
       <Route path="/add-shop" component={AddShop} />
